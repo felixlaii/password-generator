e8a9ea9dc5d17ce8b8b5ee2038886fd1ee9496d2
Felix Lai <felix.lai@hotmail.com>
2022-10-22 14:30:13 -0400

    Update App.js
    created state for passwordGen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [passwordGen, setPasswordGen] = useState({
+    length: 5,
+    uppercase: false,
+    lowercase: false,
+    numbers: false,
+    symbols: false,
+  });
   return (
     <div className="App">
   
