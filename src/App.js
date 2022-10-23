237775dde6a692e65be0caa0acc8f5b5802bbeba
Felix Lai <felix.lai@hotmail.com>
2022-10-23 00:47:09 -0400

    Update App.js
    cleaned code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [passwordGen, setPasswordGen] = useState({
@@ -9,7 +9,7 @@ function App() {
     numbers: false,
     symbols: false,
   });
-  const [handleText, setHandleText] = useState('');
+  const [handleText, setHandleText] = useState("");
   const [copied, setCopied] = useState(false);
 
   const handleChangeUppercase = () => {
@@ -47,10 +47,9 @@ function App() {
     });
   };
 
-
   function generatePassword() {
     const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+    const symbolsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
     const characterCodes = Array.from(Array(26)).map((_e, i) => i + 97);
     const lowerCaseLetters = characterCodes.map((code) =>
@@ -77,7 +76,7 @@ function App() {
       ];
       const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
       const characters = shuffleArray(availableCharacters).slice(0, length);
-      setHandelText(characters.join(''));
+      setHandelText(characters.join(""));
       return characters;
     };
 
@@ -87,9 +86,9 @@ function App() {
   return (
     <div className="wrapper">
       <div className="container wrapper-box">
-      <h2>Password Generator</h2>
-      <div className="password-box">
-      <input
+        <h2>Password Generator</h2>
+        <div className="password-box">
+          <input
             type="text"
             value={handelText}
             placeholder=""
@@ -97,7 +96,7 @@ function App() {
             onChange={(e) => setHandelText(e.target.value)}
           />
 
-<button
+          <button
             className="copy-button"
             onClick={() => {
               if (handelText.length > 0) {
@@ -109,75 +108,74 @@ function App() {
               }
             }}
           >
-            {copied ? 'Copied!' : 'Copy text'}
+            {copied ? "Copied!" : "Copy text"}
           </button>
           <br />
           <div className="word-crieteria__box">
-          <div>
-            <label>Password length</label>
-          </div>
-          <div>
-            <input
-              type="number"
-              min="4"
-              max="20"
-              value={passwordGen.length}
-              onChange={(e) => setPasswordLength(e.target.value)}
-            />
-          </div>
+            <div>
+              <label>Password length</label>
+            </div>
+            <div>
+              <input
+                type="number"
+                min="4"
+                max="20"
+                value={passwordGen.length}
+                onChange={(e) => setPasswordLength(e.target.value)}
+              />
+            </div>
           </div>
           <div className="word-crieteria__box">
-          <div>
-            <label>Include uppercase letters</label>
+            <div>
+              <label>Include uppercase letters</label>
+            </div>
+            <div>
+              <Checkbox
+                value={passwordGen.uppercase}
+                onChange={handleChangeUppercase}
+              />
+            </div>
+          </div>
+          <div className="word-crieteria__box">
+            <div>
+              <label>Include lowercase letters</label>
+            </div>
+            <div>
+              <Checkbox
+                value={passwordGen.lowercase}
+                onChange={handleChangeLowercase}
+              />
+            </div>
+          </div>
+          <div className="word-crieteria__box">
+            <div>
+              <label>Include numbers</label>
+            </div>
+            <div>
+              <Checkbox
+                value={passwordGen.numbers}
+                onChange={handleChangeNumbers}
+              />
+            </div>
+          </div>
+          <div className="word-crieteria__box">
+            <div>
+              <label>Include symbols</label>
+            </div>
+            <div>
+              <Checkbox
+                value={passwordGen.symbols}
+                onChange={handleChangeSymbols}
+              />
+            </div>
           </div>
           <div>
-            <Checkbox
-              value={passwordGen.uppercase}
-              onChange={handleChangeUppercase}
-            />
+            <button className="generate-button" onClick={generatePassword}>
+              Generate password
+            </button>
           </div>
-        </div>
-        <div className="word-crieteria__box">
-          <div>
-            <label>Include lowercase letters</label>
-          </div>
-          <div>
-            <Checkbox
-              value={passwordGen.lowercase}
-              onChange={handleChangeLowercase}
-            />
-          </div>
-        </div>
-        <div className="word-crieteria__box">
-          <div>
-            <label>Include numbers</label>
-          </div>
-          <div>
-            <Checkbox
-              value={passwordGen.numbers}
-              onChange={handleChangeNumbers}
-            />
-          </div>
-        </div>
-        <div className="word-crieteria__box">
-          <div>
-            <label>Include symbols</label>
-          </div>
-          <div>
-            <Checkbox
-              value={passwordGen.symbols}
-              onChange={handleChangeSymbols}
-            />
-          </div>
-        </div>
-        <div>
-          <button className="generate-button" onClick={generatePassword}>
-            Generate password
-          </button>
         </div>
       </div>
-      </div>
-  
     </div>
   );
 }
